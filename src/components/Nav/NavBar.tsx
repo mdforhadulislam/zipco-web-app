53fbf70d403589ba49ffcca49dbc6fece1ec3886
FOrhadul Islam <mdforhadul44@gmail.com>
2025-06-30 20:37:31 +0600

    add allll

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -8,14 +8,6 @@ import {
 } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
-import {
   Sheet,
   SheetContent,
   SheetHeader,
@@ -145,15 +137,19 @@ const Navbar = ({
             <div className="flex items-center gap-6">
               {/* Logo */}
               <a href={logo.url} className="flex items-center gap-2">
-                <Image width={100} height={100} src={logo.src} className="max-h-8" alt={logo.alt} />
+                <Image
+                  width={100}
+                  height={100}
+                  src={logo.src}
+                  className="max-h-8"
+                  alt={logo.alt}
+                />
                 <span className="text-lg font-semibold tracking-tighter">
                   {logo.title}
                 </span>
               </a>
-            
             </div>
             <div className="flex gap-2 items-center align-middle">
-              
               <div className="flex items-center gap-2">
                 Login/Sing Up <CircleUserRound className="w-9 h-9" />
               </div>
@@ -169,8 +165,8 @@ const Navbar = ({
                     <SheetTitle>
                       <a href={logo.url} className="flex items-center gap-2">
                         <Image
-                        width={100}
-                        height={100}
+                          width={100}
+                          height={100}
                           src={logo.src}
                           className="max-h-8"
                           alt={logo.alt}
@@ -200,41 +196,39 @@ const Navbar = ({
               </Sheet>
             </div>
           </nav>
-
-         
         </div>
       </div>
     </section>
   );
 };
 
-const renderMenuItem = (item: MenuItem) => {
-  if (item.items) {
-    return (
-      <NavigationMenuItem key={item.title}>
-        <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
-        <NavigationMenuContent className="bg-popover text-popover-foreground">
-          {item.items.map((subItem) => (
-            <NavigationMenuLink asChild key={subItem.title} className="w-80">
-              <SubMenuLink item={subItem} />
-            </NavigationMenuLink>
-          ))}
-        </NavigationMenuContent>
-      </NavigationMenuItem>
-    );
-  }
+// const renderMenuItem = (item: MenuItem) => {
+//   if (item.items) {
+//     return (
+//       <NavigationMenuItem key={item.title}>
+//         <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
+//         <NavigationMenuContent className="bg-popover text-popover-foreground">
+//           {item.items.map((subItem) => (
+//             <NavigationMenuLink asChild key={subItem.title} className="w-80">
+//               <SubMenuLink item={subItem} />
+//             </NavigationMenuLink>
+//           ))}
+//         </NavigationMenuContent>
+//       </NavigationMenuItem>
+//     );
+//   }
 
-  return (
-    <NavigationMenuItem key={item.title}>
-      <NavigationMenuLink
-        href={item.url}
-        className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-accent-foreground"
-      >
-        {item.title}
-      </NavigationMenuLink>
-    </NavigationMenuItem>
-  );
-};
+//   return (
+//     <NavigationMenuItem key={item.title}>
+//       <NavigationMenuLink
+//         href={item.url}
+//         className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-accent-foreground"
+//       >
+//         {item.title}
+//       </NavigationMenuLink>
+//     </NavigationMenuItem>
+//   );
+// };
 
 const renderMobileMenuItem = (item: MenuItem) => {
   if (item.items) {
